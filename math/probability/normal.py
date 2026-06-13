95a60b6bc80fee858f687c0571b1b84609bb8eb4
Karthikeyan Marimuthu <karthikmarimuthu.dev@gmail.com>
2026-06-13 15:07:55 +0200

    8,9 script updated

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -5,8 +5,10 @@
 class Normal:
     """Represents a normal distribution"""
 
+    e = 2.7182818285
+    pi = 3.1415926536
+
     def __init__(self, data=None, mean=0., stddev=1.):
-        """Initialize normal distribution"""
 
         if data is None:
 
@@ -34,33 +36,55 @@ class Normal:
                 sum(data) / len(data)
             )
 
-            variance = 0
-
-            for x in data:
-                variance += (
-                    (x - self.mean) ** 2
-                )
-
-            variance /= len(data)
+            variance = sum(
+                (x - self.mean) ** 2
+                for x in data
+            ) / len(data)
 
             self.stddev = variance ** 0.5
 
     def z_score(self, x):
-        """
-        Calculates the z-score of x
-        """
-
         return (
             (x - self.mean)
             / self.stddev
         )
 
     def x_value(self, z):
-        """
-        Calculates the x-value of z
-        """
-
         return (
             self.mean
             + z * self.stddev
         )
+
+    def pdf(self, x):
+
+        exponent = -(
+            ((x - self.mean) ** 2)
+            / (2 * (self.stddev ** 2))
+        )
+
+        return (
+            (1 /
+             (self.stddev *
+              ((2 * self.pi) ** 0.5)))
+            * (self.e ** exponent)
+        )
+
+    def cdf(self, x):
+
+        z = (
+            (x - self.mean)
+            / (self.stddev * (2 ** 0.5))
+        )
+
+        erf = (
+            (2 / (self.pi ** 0.5))
+            * (
+                z
+                - ((z ** 3) / 3)
+                + ((z ** 5) / 10)
+                - ((z ** 7) / 42)
+                + ((z ** 9) / 216)
+            )
+        )
+
+        return 0.5 * (1 + erf)
