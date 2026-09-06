f4871ee8310e3d1502875d3267a7a0e2a5d417c0
Karthikeyan Marimuthu <karthikmarimuthu.dev@gmail.com>
2026-09-06 16:56:36 +0200

    Tasks added and updated

--- supervised_learning/regularization/5-dropout_gradient_descent.py
+++ supervised_learning/regularization/5-dropout_gradient_descent.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-"""Updates the weights and biases of a neural network using gradient
-descent with L2 regularization.
+"""Updates the weights of a neural network with Dropout regularization
+using gradient descent.
 """
 import numpy as np
 
 
-def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Updates the weights and biases of a neural network using
-    gradient descent with L2 regularization.
+def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
+    """Updates the weights of a neural network with Dropout
+    regularization using gradient descent.
 
     Args:
         Y: one-hot numpy.ndarray of shape (classes, m) that contains
             the correct labels for the data
         weights: dictionary of the weights and biases of the neural
             network
-        cache: dictionary of the outputs of each layer of the neural
-            network
+        cache: dictionary of the outputs and dropout masks of each
+            layer of the neural network
         alpha: the learning rate
-        lambtha: the L2 regularization parameter
+        keep_prob: the probability that a node will be kept
         L: the number of layers of the network
 
     Returns:
-        None; the weights and biases of the network are updated in
-        place
+        None; the weights of the network are updated in place
     """
     m = Y.shape[1]
     weights_copy = {key: value.copy() for key, value in weights.items()}
@@ -32,11 +31,14 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         A_prev = cache['A' + str(i - 1)]
         W = weights_copy['W' + str(i)]
 
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+        dW = (1 / m) * np.matmul(dZ, A_prev.T)
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
 
         if i > 1:
-            dZ = np.matmul(W.T, dZ) * (1 - A_prev ** 2)
+            dA = np.matmul(W.T, dZ)
+            dA = dA * cache['D' + str(i - 1)]
+            dA = dA / keep_prob
+            dZ = dA * (1 - A_prev ** 2)
 
         weights['W' + str(i)] -= alpha * dW
         weights['b' + str(i)] -= alpha * db
